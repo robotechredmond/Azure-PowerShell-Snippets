3b3b338f69dc65a86a463a4ef1edfc7b1a0b0159
Keith Mayer <keith@keithmayer.com>
2016-09-19 11:56:58 -0400

    Updated script

---
+++ AzureRM - Azure SQL DB Server Connection Policy.ps1
@@ -0,0 +1,57 @@
+﻿# Sign-in to Azure via Azure Resource Manager
+
+    Login-AzureRmAccount
+
+# Select Azure Subscription
+
+    $subscriptionId = 
+        ( Get-AzureRmSubscription |
+            Out-GridView `
+              -Title "Select an Azure Subscription ..." `
+              -PassThru
+        ).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId
+
+# Select Azure Resource Group
+
+    $rgName =
+        ( Get-AzureRmResourceGroup |
+            Out-GridView `
+              -Title "Select an Azure Resource Group ..." `
+              -PassThru
+        ).ResourceGroupName
+
+
+# Select Azure SQL Database Server
+
+    $sqlServerName =
+        ( Get-AzureRmSqlServer `
+            -ResourceGroupName $rgName |
+            Out-GridView `
+              -Title "Select an Azure SQL Server ..." `
+              -PassThru
+        ).ServerName
+
+    $sqlServer = Get-AzureRmSqlServer `
+        -ResourceGroupName $rgName `
+        -ServerName $sqlServerName
+
+# Set Azure SQL Database Server Default Connection Policy to Proxy
+
+    $sqlServerResourceId = "/subscriptions/$subscriptionId/resourceGroups/$rgName/providers/Microsoft.Sql/servers/$sqlServerName"
+
+    $location = $sqlServer.Location
+
+    $sqlConnectionPolicyId = "$sqlServerResourceId/connectionPolicies/Default"
+
+    New-AzureRmResource `
+        -ResourceId $sqlConnectionPolicyId `
+        -Location $location `
+        -Properties @{"connectionType"="Proxy"}
+
+# Confirm configuration of Azure SQL Database Server Default Connection Policy
+
+    Get-AzureRmResource `
+        -ResourceId $sqlConnectionPolicyId
---
+++ AzureRM - Export VM Architecture.ps1
@@ -0,0 +1,67 @@
+﻿# Sign-in to Azure via Azure Resource Manager
+
+    Login-AzureRmAccount
+
+# Select Azure Subscription
+
+    $subscriptionId = 
+        ( Get-AzureRmSubscription |
+            Out-GridView `
+              -Title "Select an Azure Subdir scription ..." `
+              -PassThru
+        ).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId
+
+# Select Azure Resource Group
+
+    $rgName =
+        ( Get-AzureRmResourceGroup |
+            Out-GridView `
+              -Title "Select an Azure Resource Group ..." `
+              -PassThru
+        ).ResourceGroupName
+
+$vms = Get-AzureRMVM -ResourceGroupName $rgName 
+
+Get-AzureRmLoadBalancer -ResourceGroupName $rgName | %{ $_.BackendAddressPools.BackendIpConfigurations.Id, $_.Name }
+
+Get-AzureRmLoadBalancerBackendAddressPoolConfig
+
+$vms | % { 
+
+    $vmName = $_.Name
+    $vmLocation = $_.Location
+    $vmSize = $_.HardwareProfile.VirtualMachineSize
+    $asName = (Get-AzureRmResource -ResourceId $_.AvailabilitySetReference.ReferenceUri).Name
+    $vmNic = Get-AzureRmResource -ResourceId $_.NetworkInterfaceIDs[0]
+    $vmNicName = $vmNic.Name
+    $vmNicIpCfg = (Get-AzureRmResource -ResourceId $vmNic.Properties.IpConfigurations[0].Id -ApiVersion '2016-09-01')
+    $vmVnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[8]
+    $vmSubnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[10]
+    
+    $vmNicPrivateIp = $vmNicIpCfg.Properties.PrivateIPAddress
+    $vmNicPrivateIpAlloc = $vmNicIpCfg.Properties.PrivateIPAllocationMethod
+
+    $vmNicPublicIp = $null
+    $vmNicPublicIpAlloc = $null
+    
+    If ($vmNicIpCfg.Properties.PublicIpAddress) {
+
+        $vmNicPublicIpCfg = Get-AzureRmResource -ResourceId $vmNicIpCfg.Properties.PublicIPAddress.Id
+        $vmNicPublicIp = $vmNicPublicIpCfg.Properties.IpAddress
+        $vmNicPublicIpAlloc = $vmNicPublicIpCfg.Properties.PublicIpAllocationMethod
+
+    }
+
+    $vmNicNsgName = (Get-AzureRmResource -ResourceId $vmNic.Properties.NetworkSecurityGroup.Id).Name
+
+    $vmStorageAccountName = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/.')[2]
+    $vmStorageAccountFqdn = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/')[2]
+    $vmStorageAccountHost = (Resolve-DnsName $vmStorageAccountFqdn).NameHost.Split('.')[1]
+
+    Write-Output "$rgName,$vmName,$vmLocation,$vmSize,$asName,$vmStorageAccountName,$vmStorageAccountHost,$vmVnet,$vmSubnet,$vmNicPrivateIp,$vmNicPrivateIpAlloc,$vmNicNsgName,$vmNicPublicIp,$vmNicPublicIpAlloc" >>sprinklr-prod2.csv
+    
+}
+
