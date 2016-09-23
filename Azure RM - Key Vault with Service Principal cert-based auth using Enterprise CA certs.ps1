f51afce9c42bdcaaf841bec98acbc29bf34d7f73
Keith Mayer <keith@keithmayer.com>
2016-09-22 21:23:09 -0400

    Initial Commits

---
+++ Azure RM - Key Vault with Service Principal cert-based auth using Enterprise CA certs.ps1
@@ -0,0 +1,146 @@
+﻿# Select certificate generated from Enterprise PKI
+# Cert must have minimum RSA 2048-bit key length for Public Key
+
+    $cert = 
+        ( Get-ChildItem Cert:\CurrentUser\My |
+            Out-GridView `
+                -Title "Select a certificate ..." `
+                -PassThru
+        )
+
+    $certThumbprint = $cert.Thumbprint
+
+# Get public key and properties from selected cert
+
+    $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+
+    $keyId = [guid]::NewGuid()
+
+    $startDate = $cert.GetEffectiveDateString()
+
+    $endDate = $cert.GetExpirationDateString()
+
+# Create a Key Credential object for selected cert
+
+    Import-Module `
+        -Name AzureRM.Resources
+
+    $keyCredential = 
+        New-Object -TypeName Microsoft.Azure.Commands.Resources.Models.ActiveDirectory.PSADKeyCredential
+
+    $keyCredential.StartDate = $startDate
+
+    $keyCredential.EndDate = $endDate
+
+    $keyCredential.KeyId = $keyId
+
+    $keyCredential.CertValue = $keyValue
+
+# Define Azure AD App values for new Service Principal
+
+    $adAppName = 
+        Read-Host -Prompt “Enter unique Azure AD App name”
+
+    $adAppHomePage = 
+        Read-Host -Prompt “Enter unique Azure AD App Homepage URI”
+
+    $adAppIdentifierUri = 
+        Read-Host -Prompt “Enter unique Azure AD App Identifier URI”
+
+# Login to Azure as user credentials with Azure Subscription Owner and Azure AD Global Admin access
+
+    Login-AzureRmAccount
+
+# If more than 1 Azure subscription is present, select Azure subscription
+
+    $subscriptionId = 
+        ( Get-AzureRmSubscription |
+            Out-GridView `
+                -Title "Select an Azure Subscription ..." `
+                -PassThru
+        ).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId
+
+# Create Azure AD App object for new Service Principal
+
+    $adApp = 
+        New-AzureRmADApplication `
+            -DisplayName $adAppName `
+            -HomePage $adAppHomePage `
+            -IdentifierUris $adAppIdentifierUri `
+            -KeyCredentials $keyCredential
+
+    Write-Output “New Azure AD App Id: $($adApp.ApplicationId)”
+
+# Create Service Principal
+
+    New-AzureRmADServicePrincipal `
+        -ApplicationId $adApp.ApplicationId
+
+# Select Azure Resource Group in which to create Key Vault
+
+    $rgName =
+        (Get-AzureRmResourceGroup |
+         Out-GridView `
+            -Title "Select an Azure Resource Group ..." `
+            -PassThru).ResourceGroupName
+
+    $rg =
+        Get-AzureRmResourceGroup `
+            -Name $rgName
+
+# Create New Key Vault
+
+    $vaultName = 'MyDemoVault01'
+
+    $vaultSKU = 'Premium'
+
+    New-AzureRmKeyVault `
+        -VaultName $vaultName `
+        -ResourceGroupName $rgName `
+        -Location $rg.Location `
+        -SKU $vaultSKU
+
+# Show current properties of Key Vault
+
+    $vault =
+        Get-AzureRmKeyVault `
+            -VaultName $vaultName `
+            -ResourceGroupName $rgName
+
+    $vault | Format-List
+
+# Assign Key Vault access to new Service Principal
+
+    Set-AzureRmKeyVaultAccessPolicy `
+        -VaultName $vaultName `
+        -ServicePrincipalName $adApp.ApplicationId `
+        -PermissionsToKeys create,get,list,wrapKey,unwrapKey
+
+# Optional: Set Azure Key Vault Access Policy for ARM Template Deployments
+
+    Set-AzureRmKeyVaultAccessPolicy `
+        -VaultName $vaultName `
+        -EnabledForTemplateDeployment
+
+# Optional: Set Azure Key Vault Access Policy for ARM Compute xRP Deployments
+
+    Set-AzureRmKeyVaultAccessPolicy `
+        -VaultName $vaultName `
+        -EnabledForDeployment
+
+# Optional: If demo'ing Azure RBAC delegation, assign RBAC role to new Service Principal and test authenticating to Azure
+
+    New-AzureRmRoleAssignment `
+        -RoleDefinitionName Owner `
+        -ServicePrincipalName $adApp.ApplicationId
+
+    $tenantId = (Get-AzureRmContext).Tenant.TenantId
+
+    Login-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $tenantId `
+        -ApplicationId $adApp.ApplicationId `
+        -CertificateThumbprint $certThumbprint
--- AzureRM - Export VM Architecture.ps1
+++ AzureRM - Export VM Architecture.ps1
@@ -7,7 +7,7 @@
     $subscriptionId = 
         ( Get-AzureRmSubscription |
             Out-GridView `
-              -Title "Select an Azure Subdir scription ..." `
+              -Title "Select an Azure Subscription ..." `
               -PassThru
         ).SubscriptionId
 
@@ -23,45 +23,43 @@
               -PassThru
         ).ResourceGroupName
 
-$vms = Get-AzureRMVM -ResourceGroupName $rgName 
+    $vms = Get-AzureRMVM -ResourceGroupName $rgName 
 
-Get-AzureRmLoadBalancer -ResourceGroupName $rgName | %{ $_.BackendAddressPools.BackendIpConfigurations.Id, $_.Name }
+    $vms | % { 
 
-Get-AzureRmLoadBalancerBackendAddressPoolConfig
-
-$vms | % { 
-
-    $vmName = $_.Name
-    $vmLocation = $_.Location
-    $vmSize = $_.HardwareProfile.VirtualMachineSize
-    $asName = (Get-AzureRmResource -ResourceId $_.AvailabilitySetReference.ReferenceUri).Name
-    $vmNic = Get-AzureRmResource -ResourceId $_.NetworkInterfaceIDs[0]
-    $vmNicName = $vmNic.Name
-    $vmNicIpCfg = (Get-AzureRmResource -ResourceId $vmNic.Properties.IpConfigurations[0].Id -ApiVersion '2016-09-01')
-    $vmVnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[8]
-    $vmSubnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[10]
+        $vmName = $_.Name
+        $vmLocation = $_.Location
+        $vmSize = $_.HardwareProfile.VirtualMachineSize
+        $asName = (Get-AzureRmResource -ResourceId $_.AvailabilitySetReference.ReferenceUri).Name
+        $vmNic = Get-AzureRmResource -ResourceId $_.NetworkInterfaceIDs[0]
+        $vmNicName = $vmNic.Name
+        $vmNicIpCfg = (Get-AzureRmResource -ResourceId $vmNic.Properties.IpConfigurations[0].Id -ApiVersion '2016-09-01')
+        $vmVnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[8]
+        $vmSubnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[10]
     
-    $vmNicPrivateIp = $vmNicIpCfg.Properties.PrivateIPAddress
-    $vmNicPrivateIpAlloc = $vmNicIpCfg.Properties.PrivateIPAllocationMethod
+        $vmNicPrivateIp = $vmNicIpCfg.Properties.PrivateIPAddress
+        $vmNicPrivateIpAlloc = $vmNicIpCfg.Properties.PrivateIPAllocationMethod
 
-    $vmNicPublicIp = $null
-    $vmNicPublicIpAlloc = $null
+        $vmNicPublicIp = $null
+        $vmNicPublicIpAlloc = $null
     
-    If ($vmNicIpCfg.Properties.PublicIpAddress) {
+        If ($vmNicIpCfg.Properties.PublicIpAddress) {
 
-        $vmNicPublicIpCfg = Get-AzureRmResource -ResourceId $vmNicIpCfg.Properties.PublicIPAddress.Id
-        $vmNicPublicIp = $vmNicPublicIpCfg.Properties.IpAddress
-        $vmNicPublicIpAlloc = $vmNicPublicIpCfg.Properties.PublicIpAllocationMethod
+            $vmNicPublicIpCfg = Get-AzureRmResource -ResourceId $vmNicIpCfg.Properties.PublicIPAddress.Id
+            $vmNicPublicIp = $vmNicPublicIpCfg.Properties.IpAddress
+            $vmNicPublicIpAlloc = $vmNicPublicIpCfg.Properties.PublicIpAllocationMethod
 
+        }
+
+        $vmNicNsgName = (Get-AzureRmResource -ResourceId $vmNic.Properties.NetworkSecurityGroup.Id).Name
+
+        $vmStorageAccountName = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/.')[2]
+        $vmStorageAccountFqdn = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/')[2]
+        $vmStorageAccountHost = (Resolve-DnsName $vmStorageAccountFqdn).NameHost.Split('.')[1]
+
+    
     }
 
-    $vmNicNsgName = (Get-AzureRmResource -ResourceId $vmNic.Properties.NetworkSecurityGroup.Id).Name
+    # Get-AzureRmLoadBalancer -ResourceGroupName $rgName | %{ $_.BackendAddressPools.BackendIpConfigurations.Id, $_.Name }
 
-    $vmStorageAccountName = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/.')[2]
-    $vmStorageAccountFqdn = $_.StorageProfile.OSDisk.VirtualHardDisk[0].Uri.Split('/')[2]
-    $vmStorageAccountHost = (Resolve-DnsName $vmStorageAccountFqdn).NameHost.Split('.')[1]
-
-    Write-Output "$rgName,$vmName,$vmLocation,$vmSize,$asName,$vmStorageAccountName,$vmStorageAccountHost,$vmVnet,$vmSubnet,$vmNicPrivateIp,$vmNicPrivateIpAlloc,$vmNicNsgName,$vmNicPublicIp,$vmNicPublicIpAlloc" >>sprinklr-prod2.csv
-    
-}
-
+    # Get-AzureRmLoadBalancerBackendAddressPoolConfig
