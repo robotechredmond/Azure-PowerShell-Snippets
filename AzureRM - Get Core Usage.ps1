fbebaabd8236457d008a28bb1964c7d4a2f24172
Keith Mayer <keith@keithmayer.com>
2016-12-14 15:03:50 -0500

    Initial Commit

---
+++ AzureRM - Get Core Usage.ps1
@@ -0,0 +1,43 @@
+﻿#-------------------------------------------------------------------------
+# Copyright (c) Microsoft.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+
+# Sign-in to Azure via Azure Resource Manager
+# Note: Requires minimum of "Reader" RBAC permissions to subscription
+
+    Login-AzureRmAccount
+
+# Select Azure Subscription
+
+    $subscriptionId = 
+        ( Get-AzureRmSubscription |
+            Out-GridView `
+              -Title "Select an Azure Subscription ..." `
+              -PassThru
+        ).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId
+
+# Select Azure Region
+
+    $location = 
+        ( Get-AzureRmLocation ).Location |
+                Out-GridView `
+                  -Title "Select an Azure Subscription ..." `
+                  -PassThru
+
+# Get Azure VM current usage and quota limits
+
+    Get-AzureRmVmUsage -Location $location
