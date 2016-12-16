bdf053ab93d75e2f690914bb1dfcc008685a61fd
Keith Mayer <keith@keithmayer.com>
2016-12-16 04:01:41 -0500

    Updates

--- SQL vNext Containers Demo.ps1
+++ SQL vNext Containers Demo.ps1
@@ -1,4 +1,12 @@
-# SQL Server vNext Containers - Docker demo
+# SQL Server vNext on Windows Server Containers - Docker demo
+
+# Set variables
+$acrRegistry="kemregistry01-on.azurecr.io"
+$acrUser="kemregistry01"
+
+# Supply Passwords to use
+$saPassword=(Get-Credential).GetNetworkCredential().Password
+$acrPassword=(Get-Credential).GetNetworkCredential().Password
 
 # Search Docker Hub for MS SQL Server container images
 docker search microsoft/mssql
@@ -10,7 +18,7 @@ docker pull microsoft/mssql-server-windows
 docker images
 
 # Start a new container from the container image
-docker run -d -p 14331:1433 -e sa_password=<password> -e ACCEPT_EULA=Y microsoft/mssql-server-windows
+docker run -d -p 14331:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y microsoft/mssql-server-windows
 
 # Display running containers
 docker ps
@@ -19,18 +27,19 @@ docker ps
 docker ps -a
 
 # Get container IP address
-docker inspect –format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' container_id
+$containerID="xxxx"
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $containerID
 
-# TSQL to create sample database
+# TSQL - create sample database
 CREATE DATABASE [SampleDB01];
 GO
 USE [SampleDB01];
 GO
 CREATE TABLE dbo.Table01
 (PKID INT IDENTITY(1,1) PRIMARY KEY,
- ColA VARCHAR(10),
- ColB VARCHAR(10),
- ColC DATETIME);
+ ColA VARCHAR(10),
+ ColB VARCHAR(10),
+ ColC DATETIME);
 GO
 INSERT INTO dbo.Table01
 (ColA,ColB,ColC)
@@ -38,42 +47,41 @@ VALUES
 (REPLICATE('A',10),REPLICATE('B',10),GETUTCDATE());
 GO 10
 
-# Show records with random data in table in sample database
+# TSQL - Show records with random data in table in sample database
 SELECT * FROM dbo.Table01
 
 # Stop the container
-docker stop container_id
+docker stop $containerID
 
 # Commit the container to a new image
-docker commit container_id sampleimage01
+docker commit $containerID sampleimage01
 
 # Show list of local images
 docker images
 
 # Start new container from committed image
-docker run -d -p 14332:1433 -e sa_password=<password> -e ACCEPT_EULA=Y sampleimage01
+docker run -d -p 14332:1433 -e sa_password=$saPassword -e ACCEPT_EULA=Y sampleimage01
 
 # Login to Docker Hub
 docker login 
 
 # Login to Azure Container Registry
-docker login kemregistry01-on.azurecr.io -u kemregistry01 -p <password>
+docker login $acrRegistry -u $acrUser -p $acrPassword
 
 # Tag a container image with a repository
-docker tag sampleimage01 kemregistry01-on.azurecr.io/samples/sql:v1
+docker tag sampleimage01 ${acrRegistry}/samples/sql:v1
 
 # Show list of local images
 docker images
 
 # Push tagged container image to registry
-docker push kemregistry01-on.azurecr.io/samples/sql:v1
-
-# Logout from Registry
-docker logout
+docker push ${acrRegistry}/samples/sql:v1
 
 # Remove container images
-docker rmi sampleimage01  kemregistry01-on.azurecr.io/samples/sql:v1
+docker rmi sampleimage01  ${acrRegistry}/samples/sql:v1
 
 # Pull container image from Azure container registry
-docker pull kemregistry01-on.azurecr.io/samples/sql:v1
+docker pull ${acrRegistry}/samples/sql:v1
 
+# Logout from Registry
+docker logout $acrRegistry
