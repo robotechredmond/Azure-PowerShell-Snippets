437cdb3a202b5465484ae8cd4b2fe4c2957f479b
Keith Mayer <keith@keithmayer.com>
2017-02-27 10:31:44 -0500

    Initial commit

---
+++ AzureRM - Register NetworkWatcher Feature.ps1
@@ -0,0 +1,27 @@
+﻿# Sign-in with Azure account credentials
+
+    Login-AzureRmAccount
+
+# Select Azure Subscription
+
+    $subscriptionId = 
+        (Get-AzureRmSubscription |
+         Out-GridView `
+            -Title "Select an Azure Subscription ..." `
+            -PassThru).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId
+
+# Register NetworkWatcher Feature
+
+    Register-AzureRmProviderFeature -FeatureName AllowNetworkWatcher -ProviderNamespace Microsoft.Network
+
+# Wait until NetworkWatcher Feature is Registered
+
+do
+{
+    Start-Sleep -Seconds 60
+}
+until ( (Get-AzureRmProviderFeature -FeatureName AllowNetworkWatcher -ProviderNamespace  Microsoft.Network).RegistrationState -eq "Registered" )
+
