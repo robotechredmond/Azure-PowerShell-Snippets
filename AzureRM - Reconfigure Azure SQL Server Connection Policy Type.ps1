47fb250235254862bad6bb2577322f93a060a675
Keith Mayer <keith@keithmayer.com>
2017-04-05 07:22:25 -0400

    Updated comments and labels

--- AzureRM - Reconfigure Azure SQL Server Connection Policy Type.ps1
+++ AzureRM - Reconfigure Azure SQL Server Connection Policy Type.ps1
@@ -1,4 +1,19 @@
-﻿# Sign-in to Azure via Azure Resource Manager
+﻿#-------------------------------------------------------------------------
+# Copyright (c) Microsoft.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+
+# Sign-in to Azure via Azure Resource Manager
 
     Login-AzureRmAccount
 
@@ -41,7 +56,7 @@
             -ResourceGroupName $rgName 
         ).ServerName | 
         Out-GridView `
-            -Title "Select a VM ..." `
+            -Title "Select an Azure SQL Server instance ..." `
             -PassThru
 
 # Reconfigure Azure SQL Server connection policy type 
