49dbb9b896443e57dce3bdc43ecb071013c6751e
Keith Mayer <keith@keithmayer.com>
2017-04-16 20:15:42 -0400

    Initial commit

---
+++ AzureRM - Add Load Balancer Rules.ps1
@@ -0,0 +1,17 @@
+﻿Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionName "Contoso Sports"
+
+$rgName="sqla2-s2d-rg"
+
+$lbName = "sqla2-ilb"
+
+$lb = Get-AzureRmLoadBalancer -Name $lbName -ResourceGroupName $rgName
+
+for ($i = 20000; $i -lt 20101; $i++)
+{ 
+    $lb | Add-AzureRmLoadBalancerRuleConfig -Name "dtc-dyn${i}-rule" -FrontendIpConfigurationId $lb.FrontendIpConfigurations[0].Id -ProbeId $lb.Probes[1].Id -EnableFloatingIP -FrontendPort $i -BackendPort $i -Protocol Tcp -BackendAddressPoolId $lb.BackendAddressPools[0].Id
+}
+
+$lb | Set-AzureRmLoadBalancer
+
---
+++ AzureRM - Get Azure VM Agent Extension Versions.ps1
@@ -0,0 +1,7 @@
+﻿Login-AzureRmAccount
+
+$location = "southeastasia"
+
+(Get-AzureRmVMExtensionImage -Location $location -PublisherName “Microsoft.PowerShell” -Type “DSC”).Version
+
+(Get-AzureRmVMExtensionImage -Location $location -PublisherName “Microsoft.Azure.Security” -Type “IaaSAntimalware”).Version
