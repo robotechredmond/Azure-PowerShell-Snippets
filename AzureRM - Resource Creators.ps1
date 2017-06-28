72a2e2624c8e7d4fe31e0e7b1321722e14b6bf92
Keith Mayer <keith@keithmayer.com>
2017-06-28 08:48:12 -0400

    Initial commit

---
+++ AzureRM - Resource Creators.ps1
@@ -0,0 +1,47 @@
+﻿# Sign-in with Azure account credentials
+
+    Login-AzureRmAccount
+
+# Select Azure Subscription
+
+    $subscriptionId = 
+        (Get-AzureRmSubscription |
+         Out-GridView `
+            -Title "Select an Azure Subscription ..." `
+            -PassThru).SubscriptionId
+
+    Select-AzureRmSubscription `
+        -SubscriptionId $subscriptionId    
+
+ # Pull Activity Log History
+
+    $auditLog = $null
+
+    $currentTime = Get-Date
+
+    $endTime = $currentTime
+
+    $startTime = $endTime.AddDays(-15)
+
+    do {
+
+        $auditLog += Get-AzureRmLog -StartTime $startTime -EndTime $endTime -DetailedOutput
+
+        $endTime = $endTime.AddDays(-15)
+
+        $startTime = $startTime.AddDays(-15)
+
+    }
+
+    until ( $startTime -lt $currentTime.AddDays(-90) )
+
+# Filter Activity Log for "Created" events
+
+    $auditLog | 
+        ? Caller -like "*@*" | 
+        ? ResourceGroupName -notlike "" | 
+        ? { $_.Properties.Content.Value -Contains "Created" } |
+        Sort-Object -Property ResourceGroupName, ResourceId | 
+        Select-Object -Property ResourceGroupName, Caller, ResourceId -Unique | 
+        Format-Table -Wrap
+
