908d38c14d2473476dcec9bb284e188dec5a5f80
Keith Mayer <keith@keithmayer.com>
2018-01-24 13:07:36 -0500

    initial commit

---
+++ Install-AppInsightsStatusMonitor.ps1
@@ -0,0 +1,39 @@
+
+Configuration AppInsightsConfig
+{
+    Node localhost
+    {    
+        Script WebPi_Download
+        {
+            SetScript = 'Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/?LinkId=255386" -OutFile "${env:TEMP}\webpilauncher.exe"'
+            TestScript = '((Get-ChildItem -Path ${env:TEMP}\webpilauncher.exe -ErrorAction SilentlyContinue).Exists -eq $True)'
+            GetScript = '@{Ensure = if ((Get-ChildItem -Path ${env:TEMP}\webpilauncher.exe -ErrorAction SilentlyContinue).Exists -eq $True) {"Present"} else {"Absent"}'
+        }
+        
+        Package WebPi_Installation
+        {
+            Ensure = "Present"
+            Name = "Microsoft Web Platform Installer 5.0"
+            Path = "${env:TEMP}\wpilauncher.exe"
+            ProductId = "4D84C195-86F0-4B34-8FDE-4A17EB41306A"
+            Arguments = ""
+            DependsOn = @("[Script]WebPi_Download")
+        }
+
+        Package AppInsights_Installation
+        {
+            Ensure = "Absent"
+            Name = "Application Insights Status Monitor"
+            Path = "${env:ProgramFiles}\Microsoft\Web Platform Installer\WebPiCmd-x64.exe"
+            ProductId = ""
+            Arguments = "/install /products:ApplicationInsightsStatusMonitor /AcceptEula"
+            DependsOn = @("[Package]WebPi_Installation")
+        }
+
+    }
+
+}
+
+AppInsightsConfig
+
+Start-DscConfiguration -Path .\AppInsightsConfig -Wait -Force 
