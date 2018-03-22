c3ab8a2850e904b46d09bcfb68984f404089ed80
Keith Mayer <keith@keithmayer.com>
2018-03-22 09:49:22 -0400

    Updated to also pull AZ if configured

--- AzureRM - Export IaaS Deployment.ps1
+++ AzureRM - Export IaaS Deployment.ps1
@@ -42,91 +42,77 @@
 # Enter filename for export
 
     $reportDate = Get-Date -Format yyyyMMdd
-
-    $defaultExportFile = $filename = ".\export-${subscriptionId}-${reportDate}.csv"
-    
+    $defaultExportFile = ".\export_${subscriptionId}_${rgName}_${reportDate}.csv"
     $exportFile = Read-Host -Prompt "Export Filename (default=${defaultExportFile})"
 
     if (!$exportFile) {
-
         $exportFile = $defaultExportFile
-
     }
 
 # Export VM deployment configuration
 
-    Write-Output "rgName,vmName,vmLocation,vmSize,asName,vmFd,vmUd,vmAgentVersion,numDataDisks,vmStorageAccountName,vmStorageAccountHost,vmVnet,vmSubnet,vmNicPrivateIp,vmNicPrivateIpAlloc,vmNicNsgName,vmNicPublicIp,vmNicPublicIpAlloc" >$exportFile
+    Write-Output "rgName,vmName,vmLocation,vmSize,asName,vmFd,vmUd,vmZone,vmAgentVersion,numDataDisks,vmStorageAccountName,vmStorageAccountHost,vmVnet,vmSubnet,vmNicPrivateIp,vmNicPrivateIpAlloc,vmNicNsgName,vmNicPublicIp,vmNicPublicIpAlloc" >$exportFile
 
     [array]$vms = Get-AzureRmVM -ResourceGroupName $rgName -ErrorAction Stop
 
     $vmCount = $vms.Count
-
     $vmCurrent = 0
 
     $vms | % { 
 
         $vmCurrent++
-
         $vmRgName = $_.ResourceGroupName
-
         $vmName = $_.Name
 
         Write-Progress -Activity "Export VM configurations ..." -CurrentOperation "$vmName ..." -PercentComplete ($vmCurrent/$vmCount*100) -SecondsRemaining -1
 
         $vmLocation = $_.Location
-
         $vmSize = $_.HardwareProfile.VmSize
-
         $asName = $null
 
-        If ($_.AvailabilitySetReference.Id) {
+        if ($_.AvailabilitySetReference.Id) {
             $asName = (Get-AzureRmResource -ResourceId $_.AvailabilitySetReference.Id).Name
         }
 
+        $vmZone = $_.Zones
+
         $vmStatus = Get-AzureRmVM -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Status
-
         $vmFd = $vmStatus.PlatformFaultDomain
-
         $vmUd = $vmStatus.PlatformUpdateDomain
-
         $vmAgentVersion = $vmStatus.VMAgent.VmAgentVersion
-
-        $numDataDisks = $_.DataDiskNames.Count
-
-        $vmNic = Get-AzureRmResource -ResourceId $_.NetworkInterfaceIDs[0]
-
-        $vmNicName = $vmNic.Name
-
+        $vmNic = Get-AzureRmResource -ResourceId $_.NetworkProfile.NetworkInterfaces[0].Id
         $vmNicIpCfg = (Get-AzureRmResource -ResourceId $vmNic.Properties.IpConfigurations[0].Id -ApiVersion '2016-09-01')
-
         $vmVnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[8]
-
         $vmSubnet = $vmNicIpCfg.Properties.Subnet.Id.Split('/')[10]
-    
         $vmNicPrivateIp = $vmNicIpCfg.Properties.PrivateIPAddress
-
         $vmNicPrivateIpAlloc = $vmNicIpCfg.Properties.PrivateIPAllocationMethod
-
         $vmNicPublicIp = $null
-
         $vmNicPublicIpAlloc = $null
     
-        If ($vmNicIpCfg.Properties.PublicIpAddress) {
-
+        if ($vmNicIpCfg.Properties.PublicIpAddress) {
             $vmNicPublicIpCfg = Get-AzureRmResource -ResourceId $vmNicIpCfg.Properties.PublicIPAddress.Id
             $vmNicPublicIp = $vmNicPublicIpCfg.Properties.IpAddress
             $vmNicPublicIpAlloc = $vmNicPublicIpCfg.Properties.PublicIpAllocationMethod
-
         }
 
-        $vmNicNsgName = (Get-AzureRmResource -ResourceId $vmNic.Properties.NetworkSecurityGroup.Id).Name
+        $vmNicNsgName = $null
 
-        $vmStorageAccountName = $_.StorageProfile.OSDisk.Vhd.Uri.Split('/.')[2]
+        if ($vmNic.Properties.NetworkSecurityGroup) {
+            $vmNicNsgName = (Get-AzureRmResource -ResourceId $vmNic.Properties.NetworkSecurityGroup.Id).Name
+        }
 
-        $vmStorageAccountFqdn = $_.StorageProfile.OSDisk.Vhd.Uri.Split('/')[2]
+        $vmStorageAccountName = $null
+        $vmStorageAccountHost = $null
+        $vmStorageAccountFqdn = $null
 
-        $vmStorageAccountHost = (Resolve-DnsName $vmStorageAccountFqdn).NameHost.Split('.')[1]
+        if ($_.StorageProfile.OSDisk.Vhd.Uri) {
+            $vmStorageAccountName = $_.StorageProfile.OSDisk.Vhd.Uri.Split('/.')[2]
+            $vmStorageAccountFqdn = $_.StorageProfile.OSDisk.Vhd.Uri.Split('/')[2]
+            $vmStorageAccountHost = (Resolve-DnsName $vmStorageAccountFqdn).NameHost.Split('.')[1]
+        }
 
-        Write-Output "$vmRgName,$vmName,$vmLocation,$vmSize,$asName,$vmFd,$vmUd,$vmAgentVersion,$numDataDisks,$vmStorageAccountName,$vmStorageAccountHost,$vmVnet,$vmSubnet,$vmNicPrivateIp,$vmNicPrivateIpAlloc,$vmNicNsgName,$vmNicPublicIp,$vmNicPublicIpAlloc" -ErrorAction Stop >>$exportFile
+        $numDataDisks = $_.DataDiskNames.Count
+
+        Write-Output "$vmRgName,$vmName,$vmLocation,$vmSize,$asName,$vmFd,$vmUd,$vmZone,$vmAgentVersion,$numDataDisks,$vmStorageAccountName,$vmStorageAccountHost,$vmVnet,$vmSubnet,$vmNicPrivateIp,$vmNicPrivateIpAlloc,$vmNicNsgName,$vmNicPublicIp,$vmNicPublicIpAlloc" -ErrorAction Stop >>$exportFile
     
     }
