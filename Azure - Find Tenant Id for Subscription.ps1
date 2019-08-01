e5b438b3ba59ff285f1c8a5e8a0a45e065e96f01
Keith Mayer <keith@keithmayer.com>
2019-08-01 16:03:27 -0400

    Initial commit

---
+++ Azure - Find Tenant Id for Subscription.ps1
@@ -0,0 +1,13 @@
+﻿
+$uri = "https://management.azure.com/subscriptions/${subId}?api-version=2015-01-01"
+
+try 
+{
+    $response = Invoke-RestMethod -Method Get -Uri $uri 
+}
+catch 
+{
+    $header = $_.Exception.Response.Headers["WWW-Authenticate"]
+}
+
+$tenantId = $header.Split("/`"")[4]
---
+++ Untitled2.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzAccount
+
+$aaName = "Automate-35481b74-e090-40a8-888b-42bc552d8369-EUS"
+$rgName = "DefaultResourceGroup-EUS"
+
+Get-AzAutomationSchedule -ResourceGroupName $rgName -AutomationAccountName $aaName
